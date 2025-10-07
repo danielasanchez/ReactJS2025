c943ef85dd6531d6c9c4958134d01218082daf0e
Daniela Sánchez <danielasanchez.itt@hotmail.com>
2025-10-07 11:53:14 -0700

    Contenedores

---
+++ example2/src/App.jsx
@@ -0,0 +1,32 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+    return (
+      <>
+        <h1>JavaScript</h1>
+        <h1>React JS</h1>
+        <h1>React Native</h1>
+      </>
+  )
+}
+
+export default App
+
+
+
+/*
+Tema: Contenedores
+
+1.-El clásico es el "div" al cual se le da estilo con className
+<div className='App'>
+
+2.-También podemos usar div con style, el cual recibe un objeto de los estilos.
+<div style={{background:"gray", fontFamily:"cursive"}}>
+
+3.-Tenemos fragment
+<Fragment> </Fragment>
+
+4.- <>  </>
+
+*/ 
