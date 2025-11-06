80cdb4a7471a0bd1a76a0d5753ed61a9211106a7
Daniela Sanchez <daniela.sanchez@tectijuana.edu.mx>
2025-11-05 23:01:43 -0800

    useEffect + LocalStorage

---
+++ example15/src/Components/Encabezado.jsx
@@ -0,0 +1,14 @@
+
+
+function Encabezado({logo}){
+    return(
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Example #15
+          </p>
+        </header>
+    );
+}
+
+export default Encabezado;
